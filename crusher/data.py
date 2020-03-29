2eb485c360f9bfa4a92213c202ef85a602279f21
JudahRockLuberto <44139597+JudahRockLuberto@users.noreply.github.com>
2020-03-29 06:50:55 -0700

    Update data.py

--- crusher/data.py
+++ crusher/data.py
@@ -105,6 +105,7 @@ class BeneMassAgeZMaps(object):
         # add useful values to array
         data_dict = dict()
         for i in range(len(self.hdf5_values)):
+            print(i)
             if i in useful_values:
                 data_dict[i] = list(self.data[i])
 
