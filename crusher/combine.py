83412f55797adfe6ade6c06eb8b2bdc575c9a542
JudahRockLuberto <44139597+JudahRockLuberto@users.noreply.github.com>
2020-03-31 10:39:43 -0700

    Update combine.py

--- crusher/combine.py
+++ crusher/combine.py
@@ -1,2 +1,14 @@
 """ Used to combine high res and low res TNG maps """
 
+import numpy as np
+
+import h5py
+
+
+# use two BeneMassAgeZMaps from crusher.data as variables and return a combined map
+def combine(first_map, second_map):
+  # first, get pixel scales
+  pix_first = first_map.pix
+  pix_second = second_map.pix
+  
+  
