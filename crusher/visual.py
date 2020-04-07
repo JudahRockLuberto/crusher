2a10506dd7f69cb2c3463320ae17bf63737764a5
JudahRockLuberto <44139597+JudahRockLuberto@users.noreply.github.com>
2020-04-07 11:28:05 -0700

    Update visual.py

--- crusher/visual.py
+++ crusher/visual.py
@@ -900,7 +900,7 @@ def plot_m10_vs_m100(dict_1, dict_2, dict_3, dict_4, dict_5, dict_6, dict_7, sav
 
     ax1 = fig.add_axes([0.09, 0.09, 0.90, 0.90])
 
-    ax1.set_title(r'M10 vs M100 (11.0 < $\log\ M_{\odot}$)', fontsize=28)
+    ax1.set_title(r'M10 vs M100 (11.2 < $\log\ M_{\odot}$)', fontsize=28)
     ax1.set_ylabel(r'logM10', fontsize=28)
     ax1.set_xlabel(r'logM100', fontsize=28)
 
